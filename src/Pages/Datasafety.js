ec67bd9ed4b969e0b831512dc36c2bcb0f167d94
hereIsLucas <lucas.heroin@lernende.bbw.ch>
2023-03-28 16:54:46 +0200

    first folders with one housing my datasafety page

--- src/Pages/Datasafety.js
+++ src/Pages/Datasafety.js
@@ -15,9 +15,8 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        >Learn React</a>
+        
       </header>
     </div>
   );
