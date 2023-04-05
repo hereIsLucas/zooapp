2fdfdcde63bf1e1b2b415a70cdf576db00c271f4
Azhaar Mohamed <112543567+azhaarmohamed20@users.noreply.github.com>
2023-04-05 12:51:58 +0200

    Implement footer

---
+++ src/Components/Footer.js
@@ -0,0 +1,11 @@
+export default function Footer(){
+    return(
+        <div>
+            <footer>
+                <div>
+                    <p>Copyright ©copy: ZooApp 2023</p>
+                </div>
+            </footer>
+        </div>
+    )
+}
